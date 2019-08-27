1adaad7d16cc5c338237da65ddca6bbb7197fda6
JSG1987 <JGentry0058@gmail.com>
2019-08-27 15:03:23 -0500

    HW10

---
+++ pg83problem10/Source.cpp
@@ -0,0 +1,24 @@
+// The goal of this program is to calculate the mpg of a road trip
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	int tank = 15;
+	int distance = 375;
+	float mpg = distance / tank;
+	cout << "The MPG of a car with a 15 gallon tank traveling a distance of 375 miles is " << mpg << "miles per gallon"<< endl; 
+
+
+
+
+
+
+
+
+	system("PAUSE");
+	return 0; 
+}
